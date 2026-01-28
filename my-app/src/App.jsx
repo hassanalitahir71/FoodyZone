e56665692e6fa317116b5428b8d1739518f7988b
Hassan Ali <hassanalitahir71@gmail.com>
2026-01-28 21:23:05 +0500

    Initial commit - Foody Zone React project

---
+++ my-app/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import styled from "syled-components";
+
+function App() {
+  
+
+  return (
+    <>
+     <Maincontainer>
+       <Topcontainer>
+
+       </Topcontainer>
+     </Maincontainer>
+    </>
+  )
+}
+
+export default App
+const Maincontainer = styled.div``;
