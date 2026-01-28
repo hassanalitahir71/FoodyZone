9a8c2ec70ad32ffa9f5006fcc5c6f5cea0d8113e
Hassan Ali <hassanalitahir71@gmail.com>
2026-01-29 01:31:20 +0500

    feat: add homepage layout and styling with styled-components

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import styled from "syled-components";
+import styled from "styled-components";
 
 function App() {
   
@@ -11,12 +11,74 @@ function App() {
     <>
      <Maincontainer>
        <Topcontainer>
-
+        <div className="nav" >
+        <img src="/images/logo.svg" alt="" className="logo" />
+        <input type="text" placeholder='Search Food....' />
+         </div>
+         <div className="selection">
+          <button>All</button>
+          <button>BreakFast</button>
+          <button>Lunch</button>
+          <button>Dinner</button>
+         </div>
        </Topcontainer>
+       <BottomContainer>
+        
+       </BottomContainer>
      </Maincontainer>
     </>
   )
 }
 
 export default App
-const Maincontainer = styled.div``;
+const Maincontainer = styled.div`
+  background: black;
+  height: 100vh;
+  max-width: 1440px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+`;
+const Topcontainer =styled.section`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+background: #1c1b1b;
+height: 130px;
+gap: 35px;
+
+.nav {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 1000px;
+}
+  input{
+    background:  transparent;
+    color: white;
+    border : red 1px solid;
+    padding: 10px;
+    border-radius: 5px;
+  }
+ .selection{
+  display :flex;
+  gap: 14px;
+
+  button{
+    background-color: red;
+    color: white;
+    border : none;
+    padding: 7px;
+    border-radius: 2px;
+    height: 25px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }}`
+const BottomContainer = styled.div`
+  background-image: url("/images/bg.png");
+  flex:1;
+
+`;
+
--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -2,10 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createGlobalStyle } from 'styled-components'
 
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin : 0;
+    padding: 0;
+
+    
+  }
+`
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <GlobalStyles/>
     <App />
   </StrictMode>,
 )
