fc6d74175f459e0fe955dcdb7d0550ec93463bce
Hassan Ali <hassanalitahir71@gmail.com>
2026-02-01 02:07:20 +0500

    feat: add food category filtering and improve UI design

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,21 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { createGlobalStyle } from 'styled-components'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
   * {
     margin : 0;
     padding: 0;
-
     
   }
-`
+  body {
+    font-family: 'Inter', sans-serif;
+  }
+`;
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GlobalStyles/>
+    <GlobalStyles />
     <App />
   </StrictMode>,
-)
+);
